7cd2633373f34de0e0a0fb47ba2c8a8861018ed5
zhangchao <chaozhang2000@outlook.com>
2024-03-20 22:57:00 +0800

    update conv3 kernel.h and param.json

--- kernels/conv3/kernel.h
+++ kernels/conv3/kernel.h
@@ -6,14 +6,14 @@ int conv3data3[20] ={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 int conv3result[20];
 
 void loaddataforkernel(CGRA* cgra){
-	cgra->nodes[0][0]->datamem->writeDatas(conv3data1,0,20);
-	cgra->nodes[0][1]->datamem->writeDatas(conv3data2,0,20);
-	cgra->nodes[0][2]->datamem->writeDatas(conv3data3,0,20);
+	cgra->datamems[0]->writeDatas(conv3data1,0,20);
+	cgra->datamems[1]->writeDatas(conv3data2,0,20);
+	cgra->datamems[2]->writeDatas(conv3data3,0,20);
 }
 void printresult(CGRA*cgra){
 	std::cout<<"Print result:"<<std::endl;
 	for(int i = 1; i<=18;i++){
-		std::cout<<"address = "<<i<<" data = "<<cgra->nodes[0][3]->datamem->readData(i)<<std::endl;
+		std::cout<<"address = "<<i<<" data = "<<cgra->datamems[3]->readData(i)<<std::endl;
 	} 
 }
 
@@ -31,7 +31,7 @@ void checkresult(CGRA*cgra){
   		conv3kernel(conv3data1,conv3data1,conv3data1,conv3result,i);
   }
 	for(i = 1; i<19;i++){
-			if(conv3result[i] != cgra->nodes[0][3]->datamem->readData(i)){
+			if(conv3result[i] != cgra->datamems[3]->readData(i)){
 				std::cout<<"sim result is different from the theoretical results"<<std::endl;
 				std::cout<<"Sim not PASS!"<<std::endl;
 				return;
